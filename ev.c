d08764f1bb70081b09885cf52cfb015bf558bed4
klHaritha <48052673+klHaritha@users.noreply.github.com>
2019-03-12 05:34:06 +0530

    Create ev.c

---
+++ ev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main()
+{
+   int a;
+   
+    printf("enter num:");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("even num");
+        
+    }
+    else
+    {
+        printf("odd num");
+        
+    }
+
+    
+}
